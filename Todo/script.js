f3e7c57e0b4a91e823b99226188326e0ec6093b0
Dhruvikankadiya1337 <dhruvikankadiya@gmail.com>
2025-04-02 17:51:38 +0530

    Update script.js

--- Todo/script.js
+++ Todo/script.js
@@ -1,24 +1,32 @@
-$(document).ready(
-  function(){
-      $('#button').click(
-          function(){
-              var toAdd = $('input[name=ListItem]').val();
-               $('ol').append('<li>' + toAdd + '</li>');
-          });
-     
-     $("input[name=ListItem]").keyup(function(event){
-        if(event.keyCode == 13){
+
+$(document).ready(function () {
+  $('#button').click(function () {
+      var toAdd = $('input[name=ListItem]').val().trim();
+
+      if (toAdd !== listItem) {
+          var currentDateTime = new Date().toLocaleString();
+
+          var listItem = '<li>' +
+              '<input type="radio" class="task-radio"> ' +  '<span class="task-text">' + toAdd + '</span>' +
+              ' <span class="task-time">(' + currentDateTime + ')</span>' +
+            '</li>';
+
+          $('ol').prepend(listItem);
+          $('input[name=ListItem]').val('');
+      }
+  });
+
+  $("input[name=ListItem]").keyup(function (event) {
+      if (event.keyCode == 13) {
           $("#button").click();
-        }         
-    });
-    
-    $(document).on('dblclick','li', function(){
-      $(this).toggleClass('strike');    
-    });
-    
-    $('input').focus(function() {
+      }
+  });
+
+  $(document).on('change', '.task-radio', function () {
+      $(this).siblings('.task-text').toggleClass('strike');
+  });
+
+  $('input[name=ListItem]').focus(function () {
       $(this).val('');
-    });
-     
-  }
-);
+  });
+});
